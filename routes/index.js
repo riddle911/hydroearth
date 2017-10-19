802669d57435fd6c7241082aaa86d0f0b35a0ed2
riddle911 <riddle911@foxmail.com>
2017-10-19 21:51:10 +0800

    10.19athome

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Map page. */
+router.get('/map', function(req,res) {
+  res.render('map');
+});
+
+module.exports = router;
